f242817823425ef3024604cfcd35ab94200add3e
ribeiro-dev <romilson.2r@gmail.com>
2020-09-03 12:01:29 -0300

    finish functions to play

--- script.js
+++ script.js
@@ -8,20 +8,64 @@ function computerPlay() {
     return choosedOption
 }
 
-console.log(computerPlay())
-
-
 function playRound(playerSelection, computerSelection) {
-    // Return the winner of the round
+    // Returns the winner of the round
 
-
-    conditions = {
-        "rock": [""]
-    }
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+    
     // If player chooses rock
+    if (playerSelection === "rock") {
+        if (computerSelection === "scissors") { // If he wins
+            return "You win! Rock beats Scissors"
+            
+        } else if (computerSelection === "paper") { // If he loses  
+            return "You lose! Paper beats Rock"
 
+        } else if (computerSelection === playerSelection) {
+            return "It's a tie!"
+        }
+    }
 
     // If player chooses paper
+    else if (playerSelection === "paper") {
+        
+        if (computerSelection === "rock") { // If he wins  
+            return "You win! Paper beats Rock"
+
+        } else if (computerSelection === "scissors") { // If he loses
+            return "You lose! Scissors beats Paper"
+
+        } else if (computerSelection === playerSelection) {
+            return "It's a tie!"
+        }
+    }
 
     // If player chooses scissors
+    else if (playerSelection === "scissors") {
+        
+        if (computerSelection === "paper") { // If he wins  
+            return "You win! Scissors beats Paper"
+
+        } else if (computerSelection === "rock") { // If he loses
+            return "You lose! Rock beats Scissors"
+
+        } else if (computerSelection === playerSelection) {
+            return "It's a tie!"
+        }
+    }
+
+    else {
+        return alert("Invalid answer! Try again")
+    }
 }
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let player = prompt("Choose rock, paper or scissors")
+        let cpu = computerPlay()
+        console.log(playRound(player, cpu))
+    }
+}
+
+game()
